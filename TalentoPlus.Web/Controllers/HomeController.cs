19af74fd1805ab72560a7023fbea1b0861f9ea61
Contrerinhaz <englosopoldo@gmail.com>
2025-12-09 16:13:02 -0500

    feat: implement authentication, admin role and login ui

--- TalentoPlus.Web/Controllers/HomeController.cs
+++ TalentoPlus.Web/Controllers/HomeController.cs
@@ -4,23 +4,27 @@ using TalentoPlus.Web.Models;
 
 namespace TalentoPlus.Web.Controllers;
 
-public class HomeController : Controller {
+public class HomeController : Controller
+{
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger) {
+    public HomeController(ILogger<HomeController> logger)
+    {
         _logger = logger;
     }
 
-    public IActionResult Index() {
-        return View();
-    }
-
-    public IActionResult Privacy() {
-        return View();
+    public IActionResult Index()
+    {
+        if (!User.Identity?.IsAuthenticated ?? true)
+        {
+            return RedirectToAction("Login", "Account");
+        }
+        return RedirectToAction("Index", "Employees");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error() {
+    public IActionResult Error()
+    {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
--- TalentoPlus.Web/Program.cs
+++ TalentoPlus.Web/Program.cs
@@ -1,12 +1,68 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TalentoPlus.Web.Data;
+using TalentoPlus.Web.Repositories.Implementations;
+using TalentoPlus.Web.Repositories.Interfaces;
+using TalentoPlus.Web.Services.Implementations;
+using TalentoPlus.Web.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 3;
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+    options.AccessDeniedPath = "/Account/AccessDenied";
+});
+
 builder.Services.AddControllersWithViews();
+
+// Register Repositories
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+
+// Register Services
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IDepartmentService, DepartmentService>();
+builder.Services.AddScoped<IExcelService, ExcelService>();
 
 var app = builder.Build();
 
+// Seed Data
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        await DbSeeder.SeedRolesAndAdminAsync(services);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while seeding the database.");
+    }
+}
+
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment()){
+if (!app.Environment.IsDevelopment())
+{
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
@@ -20,7 +76,8 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-name: "default",
-pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
